a6c5e6cae0b384e41c5c2c805766a9834bbebe17
Lucky-1300 <luckykumari42774@gmail.com>
2025-12-05 21:44:34 +0530

    madethe UI

--- todo app/src/TodoApp.jsx
+++ todo app/src/TodoApp.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 
 export const TodoApp = () => {
   return (
-    <div>TodoApp</div>
+    <div className='container'>
+      <h1 className="heading">My Tasks</h1>
+      <p className='para'>Keep track of your daily tasks</p>
+
+      <div className='container2'>
+        <div className='container3'>
+          <input className="input-box" placeholder='Add a new task..' />
+          <button className='btn'>+</button>
+        </div>
+
+        <div>
+          <label>
+            <input type="checkbox" />
+            Task 1
+          </label>
+
+          <br />
+
+          <label>
+            <input type="checkbox" />
+            Task 2
+          </label>
+        </div>
+
+      </div>
+    </div>
   )
 }
